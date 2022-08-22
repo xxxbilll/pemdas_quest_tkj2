057600160b86f7db04b9409674e1dac942a65b86
xxxbilll <111479823+xxxbilll@users.noreply.github.com>
2022-08-21 23:41:01 -0700

    Add files via upload

---
+++ python4-nabil.py
@@ -0,0 +1,22 @@
+print("1. segitiga")
+
+alas = int(input("masukan nilai alas"))
+tinggi = int(input("masukan nilai tinggi"))
+luas_s = alas * tinggi /2
+
+print("luas segitiga adalah", luas_s)
+
+print("2. persegi\n")
+
+sisi = int(input("masukan nilai sisi: "))
+luas_p = sisi * sisi 
+
+print("luas persegi adalah", luas_p)
+
+print("3. persegi panjang")
+
+panjang = int(input("masukan nilai panjang"))
+lebar = int(input("masukan nilai lebar: "))
+
+print("luas persegi panjang adalah", luas_p)
+
